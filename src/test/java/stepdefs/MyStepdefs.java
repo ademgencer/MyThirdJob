8088115094396af9d9ed128d1a1b5c1e3134e78b
ademgencer <ademyasargencer@gmail.com>
2023-05-06 12:00:50 +0300

    MyBranch oluşturuldu

--- src/test/java/stepdefs/MyStepdefs.java
+++ src/test/java/stepdefs/MyStepdefs.java
@@ -1,10 +1,16 @@
 package stepdefs;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class MyStepdefs {
     @Given("deneme")
     public void deneme() {
         System.out.println("Konsol Çıktısı : My Third Job");
+    }
+
+    @When("ikinci çıktıyı verelim")
+    public void ikinciÇıktıyıVerelim() {
+        System.out.println("myBranch te oluşturuldu");
     }
 }
